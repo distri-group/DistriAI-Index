02aca3aba97620ac51bf7fabb1f31265e89f3d56
zhangyhSir <zyh888686@gmail.com>
2024-04-29 10:11:18 +0800

    optimization model list query

--- handlers/ai_model.go
+++ handlers/ai_model.go
@@ -107,8 +107,12 @@ func ModelList(context *gin.Context) {
 	}
 
 	var response ModelListResponse
-	aiModel := model.AiModel{Type1: req.Type1, Type2: req.Type2}
-	tx := common.Db.Model(&aiModel).Where(&aiModel)
+	//aiModel := model.AiModel{Type1: req.Type1, Type2: req.Type2}
+	//tx := common.Db.Model(&aiModel).Where(&aiModel)
+	tx := common.Db.Table("ai_models").
+		Select("ai_models.id, ai_models.owner, ai_models.name, ai_models.framework, ai_models.license, ai_models.type1, ai_models.type2, ai_models.tags, ai_models.created_at, ai_model_heats.downloads, ai_model_heats.likes").
+		Joins("INNER JOIN ai_model_heats ON ai_models.owner = ai_model_heats.owner AND ai_models.name = ai_model_heats.name").
+		Where("ai_models.type1 = ? AND ai_models.type2 = ?", req.Type1, req.Type2)
 	if "" != req.Name {
 		name := strings.ReplaceAll(req.Name, "%", "\\%")
 		tx.Where("name LIKE ?", "%"+name+"%")
